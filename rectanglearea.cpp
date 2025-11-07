3e923412ec361ecf934da438aa363e0e2de9f4be
peter mugambi <petermugambi552@gmail.com>
2025-11-07 12:26:03 +0300

    Add files via upload

---
+++ rectanglearea.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // create integer variables length and width
+    int length = 40;
+    int width = 60;
+    // calculate area of a rectangle
+    int area = length * width;
+    // print the variables
+    cout << "length is:" << length << "\n";
+    cout << "width is:" << width << "\n";
+    cout << "area of the rectangle is:" << area << "\n";
+    return 0;
+}
