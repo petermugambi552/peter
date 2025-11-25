bc4d8059c14c6a47c1ead5a56b7778c811ad699f
peter mugambi <petermugambi552@gmail.com>
2025-11-25 18:19:05 +0300

    Add files via upload

---
+++ recursivefibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count;
+    cout << "Enter number of Fibonacci terms: ";
+    cin >> count;
+
+    int t1 = 0, t2 = 1, nextTerm = 0;
+    cout << "Fibonacci Series: " << endl;
+    for (int i = 1; i <= count; ++i)
+    {
+        return 0;
+    }
+}
